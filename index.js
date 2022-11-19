5321febb6ef5367532505e877fd7685c38bc7ebc
Alex-Shake <me@aleksshake.ru>
2022-11-19 04:51:52 +0300

    func5

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 function func1(num) {
     let square = num**2;
-    return square;
+    console.log(square);
 }
 
 function func2(num1, num2) {
     let summ = num1+num2;
-    return summ;
+    console.log(summ);
 }
 
+function func3(num1, num2, num3) {
+    let calc = (num1-num2)/num3;
+    console.log(calc);
+}
+
+function func4(day) {
+    switch (day) {
+        case 1:
+            console.log('Понедельник');
+            break;
+        case 2:
+            console.log('Вторник');
+            break;
+        case 3:
+            console.log('Среда');
+            break;
+        case 4:
+            console.log('Четверг');
+            break;
+        case 5:
+            console.log('Пятница');
+            break;
+        case 6:
+            console.log('Суббота');
+            break;
+        case 7:
+            console.log('Воскресенье');
+            break;
+    }
+}
+
+function func5(num1, num2) {
+    if (+num1 == +num2) {
+        console.log(true);
+    } else {console.log(false);}
+}
+
+func5(1, 1)
